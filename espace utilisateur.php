b9e0508bf7c28f66d69fe96c54b171333110e223
jules-fogeron <jules.fogeron@lyceeastier.com>
2024-01-25 10:18:42 +0100

    add fill espace utilisateur

---
+++ espace utilisateur.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Espace Utilisateur</title>
+    <link rel="stylesheet" href="css/espace_utilisateur.css">
+</head>
+<body>
+<?php /*include("header.php") */?>
+<div class="espace-utilisateur">
+    <div class="frame-titre">
+        <div class="historique">Historique</div>
+    </div>
+    <div class="frame-filtre">
+        <div class="tout">Tout</div>
+        <div class="effectu">effectué</div>
+        <div class="en-cours">en cours</div>
+        <div class="venir">à venir</div>
+    </div>
+    <div class="frame-tableau">
+        <div class="frame-info">
+            <div class="motif-du-s-jour">Motif du Séjour</div>
+            <div class="date">Date</div>
+            <div class="m-decin">Médecin</div>
+            <div class="sp-cialit">Spécialité</div>
+            <div class="statue">Statue</div>
+        </div>
+        <div class="frame-17">
+            <div class="motif-du-s-jour2">Motif du Séjour</div>
+            <div class="date2">Date</div>
+            <div class="m-decin2">Médecin</div>
+            <div class="sp-cialit2">Spécialité</div>
+            <div class="statue2">Statue</div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
