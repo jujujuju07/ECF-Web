1966a68e5f43f23dc0d6688d9d32a0409597c224
jules-fogeron <jules.fogeron@lyceeastier.com>
2024-01-25 09:36:53 +0100

    add fill inscription

---
+++ inscription.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="css/inscription.css">
+</head>
+<body>
+<?php include("header.php") ?>
+
+<div class="cr-ation-d-un-compte-utilisateur">
+    <div class="frame-info">
+        <div class="group-e-mail-mot-de-passe">
+            <div class="group-mot-de-passe">
+                <div class="frame-mot-de-passe"></div>
+                <div class="mot-de-passe">Mot de Passe</div>
+            </div>
+            <div class="group-e-mail">
+                <div class="frame-e-mail"></div>
+                <div class="e-mail">E-Mail</div>
+            </div>
+        </div>
+        <div class="group-nom-pr-nom">
+            <div class="group-pr-nom">
+                <div class="frame-pr-nom"></div>
+                <div class="pr-nom">Prénom</div>
+            </div>
+            <div class="group-nom">
+                <div class="frame-nom"></div>
+                <div class="nom">Nom</div>
+            </div>
+        </div>
+        <div class="group-adresse-postale">
+            <div class="frame-adresse-postale"></div>
+            <div class="adresse-postale">Adresse postale</div>
+        </div>
+    </div>
+    <div class="frame-button">
+        <div class="s-inscrire">S’inscrire</div>
+    </div>
+</div>
+
+</body>
+</html>
