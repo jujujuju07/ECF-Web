902e769847d0d33bc548387f068b731ceb458020
jules-fogeron <jules.fogeron@lyceeastier.com>
2024-01-25 09:17:07 +0100

    add file connexion

---
+++ connexion.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="css/connexion.css">
+</head>
+<body>
+<?php include("header.php") ?>
+<div class="connexion-compte-utilisateur">
+    <div class="frame-info">
+        <div class="group-e-mail">
+            <div class="frame-e-mail"></div>
+            <div class="e-mail">E-Mail</div>
+        </div>
+        <div class="group-mot-de-passe">
+            <div class="frame-mot-de-passe"></div>
+            <div class="mot-de-passe">Mot de Passe</div>
+        </div>
+    </div>
+    <div class="frame-button">
+        <div class="frame-se-connecter">
+            <div class="se-connecter">Se Connecter</div>
+        </div>
+        <div class="frame-s-inscrire">
+            <div class="s-inscrire">S’inscrire</div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
