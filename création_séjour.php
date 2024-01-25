450ac284e4ba6baaf79e45e808d9ef2866f0434e
jules-fogeron <jules.fogeron@lyceeastier.com>
2024-01-25 10:37:57 +0100

    add fill création_séjour

---
+++ création_séjour.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Création Séjour</title>
+    <link rel="stylesheet" href="css/création_séjour.css">
+</head>
+<body>
+<?php include("header_connecter.php") ?>
+
+<div class="creation-d-un-sejour">
+    <div class="frame-info">
+        <div class="frame-date-motif-du-sejour">
+            <div class="frame-date">
+                <div class="date">Date</div>
+                <div class="frame-date2"></div>
+            </div>
+            <div class="frame-motif-du-sejour">
+                <div class="motif-du-s-jour">Motif du Séjour</div>
+                <div class="frame-motif-du-s-jour2"></div>
+            </div>
+        </div>
+        <div class="frame-specialit-necessaire-medecin">
+            <div class="frame-specialit-necessaire">
+                <div class="specialit-necessaire">Spécialité Nécessaire</div>
+                <div class="frame-specialit-necessaire2"></div>
+            </div>
+            <div class="frame-medecin">
+                <div class="medecin">Médecin</div>
+                <div class="frame-medecin2"></div>
+            </div>
+        </div>
+    </div>
+    <div class="frame-button">
+        <div class="confirmation">Confirmation</div>
+    </div>
+</div>
+
+</body>
+</html>
