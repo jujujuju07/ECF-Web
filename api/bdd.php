d16d1db621a4072b3bbc80d553c800996dc7be10
jules-fogeron <jules.fogeron@lyceeastier.com>
2024-02-13 15:42:53 +0100

    rename database BDD

--- api/bdd.php
+++ api/bdd.php
@@ -8,7 +8,7 @@ class bdd
         $hostname_BDD = "localhost";
         $username_BDD = "ECF";
         $password_BDD = "ECFecfECF";
-        $database_BDD = "ecf";
+        $database_BDD = "foju5697_ecf";
         try {
             $this->conn = new PDO("mysql:host=$hostname_BDD;dbname=$database_BDD", $username_BDD, $password_BDD);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
