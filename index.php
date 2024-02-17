f3bc65a1a0e90227a19ed4f02846726d1b518b7c
jules-fogeron <jules.fogeron@lyceeastier.com>
2024-02-17 23:06:22 +0100

    new design

--- index.php
+++ index.php
@@ -8,10 +8,26 @@
 <body>
 <?php include("header.php") ?>
 <div class="page-d-accueil">
-    <div
-            class="text"
-    >
+    <div class="text">
         SoigneMoi, est un hôpital de la région lilloise (dans le nord de la France)
+    </div>
+    <img src="image/lille.jpg" alt="image de lille" class="image">
+    <div class="liste">
+        <label class="label-titre-liste">liste offre de soins</label>
+        <div class="liste-offre">
+            <div class="liste-liste-offre-1">
+                <label class="label-liste">Chirurgie</label>
+                <label class="label-liste">Femme - Mère - Enfant</label>
+                <label class="label-liste">Gériatrie</label>
+                <label class="label-liste">EHPAD</label>
+            </div>
+            <div class="liste-liste-offre-2">
+                <label class="label-liste">Médecine</label>
+                <label class="label-liste">Spécialités Externes</label>
+                <label class="label-liste">Services Médico-Techniques</label>
+                <label class="label-liste">Autres Services de Soins</label>
+            </div>
+        </div>
     </div>
 </div>
 
